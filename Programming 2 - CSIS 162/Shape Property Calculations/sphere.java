6e15d40f3e07a87f593133b15266b422258374c2
Brady Lange <brady.lange111@gmail.com>
2018-02-10 14:50:04 -0600

    Create sphere.java

---
+++ Programming 2 - CSIS 162/Shape Property Calculations/sphere.java
@@ -0,0 +1,61 @@
+
+public class sphere { //start of sphere class
+	public double area, volume, circumference, radius;
+	
+	public sphere() //default constructor
+	{
+		radius = 0;
+	}
+	
+	public sphere(double rad) //alternate constructor
+	{
+		radius = rad;
+	}
+	
+	public void calcArea() //area calculation method
+	{
+		area = 	4 * Math.PI * (radius * radius);
+	}
+	
+	public void calcVolume() //volume calculation method
+	{
+		volume = 	(4/3.0) * Math.PI * (radius * radius * radius);
+	}
+	
+	public void calcCircum() //cirumference calcultion method
+	{
+		circumference = 2 * Math.PI * radius;
+	}
+	
+	public void setSphRad(double rad) //setter method
+	{
+		radius = rad;
+	}
+	
+	public double getSphRad() //getter method
+	{
+		return radius;
+	}
+	
+	public double getSphArea() //getter method
+	{
+		return area;
+	}
+	
+	public double getSphCircum() //getter method
+	{
+		return circumference;
+	}
+	
+	public double getSphVol() //getter method
+	{
+		return volume;
+	}
+	
+	public String toString() //toString method
+	{
+		String data = "Circumference = " + circumference + " Area = " + area + " Volume = " + volume + " Radius: " + radius;
+		return data;
+	}
+	
+} //end of sphere class
