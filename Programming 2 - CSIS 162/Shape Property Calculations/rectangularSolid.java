41181c947a6c43adc29e75856771322460265443
Brady Lange <brady.lange111@gmail.com>
2018-02-10 14:48:43 -0600

    Create rectangularSolid.java

---
+++ Programming 2 - CSIS 162/Shape Property Calculations/rectangularSolid.java
@@ -0,0 +1,86 @@
+
+public class rectangularSolid {
+
+	public double area, volume, perimeter, length, width, height;
+	
+	public rectangularSolid() //default constructor
+	{
+		length = 0; 
+		width = 0;
+		height = 0;
+	}
+	
+	public rectangularSolid(double len, double wid, double hei) //alternate constructor 
+	{
+		length = len;
+		width = wid;
+		height = hei;
+	}
+	
+	public void calcArea() //area calculation method
+	{
+		area = 2 * (width * length + height * length + height * width);
+	}
+	
+	public void calcVolume() //volume calculation method
+	{
+		volume = length * width * height;
+	}
+	
+	public void calcPerimeter() //perimeter calculation method
+	{
+		perimeter = 2 * width + 2 * length;
+	}
+	
+	public void setRecSolLen(double len) //setter method
+	{
+		length = len;
+	}
+	
+	public void setRecSolWid(double wid) //setter method
+	{
+		width = wid;
+	}
+	
+	public void setRecSolHei(double hei) //setter method
+	{
+		height = hei;
+	}
+	
+	public double getRecSolLen() //getter method
+	{
+		return length;
+	}
+	
+	public double getRecSolWid() //getter method
+	{
+		return width;
+	}
+	
+	public double getRecSolHei() //getter method
+	{
+		return height;
+	}
+	
+	public double getRecSolArea() //getter method
+	{
+		return area;
+	}
+	
+	public double getRecSolPeri() //getter method
+	{
+		return perimeter;
+	}
+	
+	public double getRecSolVol() //getter method
+	{
+		return volume;
+	}
+	
+	public String toString() //toString method
+	{
+		String data = "Perimeter =  " + perimeter + " Area = " + area + " Length: " + length + " Width: " + width + " Height: " + height;
+		return data;
+	}
+	
+} //end of rectangular solid class
