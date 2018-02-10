1b191a41016d355987ed759cc9589c43828d788f
Brady Lange <brady.lange111@gmail.com>
2018-02-10 00:58:27 -0600

    Update Cell Phone Service Bill Calculator.java

--- Programming 1 - CSIS 161/Cell Phone Service Bill Calculator.java
+++ Programming 1 - CSIS 161/Cell Phone Service Bill Calculator.java
@@ -1,11 +1,10 @@
 /*
  * Brady Lange
  * CSIS 161 Programming
+ * This program calculates the phone bill's of 2 types of services from a telephone company.
+ * Program #5
  * Section: 02
  * Meets: M,W, & F at 1:00 PM to 1:50 PM.
- */
-/*
- * This program calculates the phone bill's of 2 types of services from a telephone company.
  */
 
 import java.util.*; //importing already coded code
@@ -14,123 +13,123 @@ public class cellBill { //start of public class
 
 	public static void main(String[] args) { //start of main method
 
-Scanner console = new Scanner(System.in);	//to be able to use the code	
-		
-//Declaring variables
-String name;
-final int accountNum;
-String service;
+		Scanner console = new Scanner(System.in);	//to be able to use the code	
 
-//Regular service variables
-String reg = "REGULAR";
-final int freeRegMins = 50;
-final double regFee = 10.00;
-double regCharge;
-String rChargeForm;
+		//Declaring variables
+		String name;
+		final int accountNum;
+		String service;
 
-//Premium service variables
-int dayMins; //this will also work for the regular service statement
-int nightMins;
-String prem = "PREMIUM";
-final int freePremDayMins = 75;
-final int freePremNightMins = 100;
-final double premFee = 25.00;
-double premCharge;
-String pChargeForm;
+		//Regular service variables
+		String reg = "REGULAR";
+		final int freeRegMins = 50;
+		final double regFee = 10.00;
+		double regCharge;
+		String rChargeForm;
 
-//Users inputs
-System.out.print("What is your first name? ");
-name = console.next();
-System.out.print("What is your account number? ");
-accountNum = console.nextInt();
-System.out.print("What is your service type (Regular or Premium)? ");
-service = console.next();
+		//Premium service variables
+		int dayMins; //this will also work for the regular service statement
+		int nightMins;
+		String prem = "PREMIUM";
+		final int freePremDayMins = 75;
+		final int freePremNightMins = 100;
+		final double premFee = 25.00;
+		double premCharge;
+		String pChargeForm;
 
-//Capitalizing users input
-String capService = service.toUpperCase();
+		//Users inputs
+		System.out.print("What is your first name? ");
+		name = console.next();
+		System.out.print("What is your account number? ");
+		accountNum = console.nextInt();
+		System.out.print("What is your service type (Regular or Premium)? ");
+		service = console.next();
 
-//Using if and else statements to execute which service is being used
-if (service.equalsIgnoreCase(prem)) { //ignoring case sensitive 
-System.out.print("How many night minutes did you use? (No decimals) ");
-nightMins = console.nextInt();
-System.out.print("How many day minutes did you use? (No decimals) ");
-dayMins = console.nextInt();
+		//Capitalizing users input
+		String capService = service.toUpperCase();
 
-if (dayMins > freePremDayMins && nightMins > freePremNightMins) {
-premCharge = premFee + (nightMins - freePremNightMins) * 0.05 + (dayMins - freePremDayMins) * 0.10;
-pChargeForm = String.format("%.2f", premCharge); //formatting the decimal to two places
-System.out.println("Name: " + name);
-System.out.println("Account number: " + accountNum);
-System.out.println("Service type: " + capService);
-System.out.println("Service minutes used (Night): " + nightMins);
-System.out.println("Service minutes used (Day): " + dayMins);
-System.out.println("Amount charged: $" + pChargeForm);
-}		
+		//Using if and else statements to execute which service is being used
+		if (service.equalsIgnoreCase(prem)) { //ignoring case sensitive 
+			System.out.print("How many night minutes did you use? (No decimals) ");
+			nightMins = console.nextInt();
+			System.out.print("How many day minutes did you use? (No decimals) ");
+			dayMins = console.nextInt();
 
-if (dayMins < freePremDayMins && nightMins < freePremNightMins) {
-premCharge = premFee;
-pChargeForm = String.format("%.2f", premCharge); //formatting the decimal to two places
-System.out.println("Name: " + name);
-System.out.println("Account number: " + accountNum);
-System.out.println("Service type: " + capService);
-System.out.println("Service minutes used (Night): " + nightMins);
-System.out.println("Service minutes used (Day): " + dayMins);
-System.out.println("Amount charged: $" + pChargeForm);
-}
+			if (dayMins > freePremDayMins && nightMins > freePremNightMins) {
+				premCharge = premFee + (nightMins - freePremNightMins) * 0.05 + (dayMins - freePremDayMins) * 0.10;
+				pChargeForm = String.format("%.2f", premCharge); //formatting the decimal to two places
+				System.out.println("Name: " + name);
+				System.out.println("Account number: " + accountNum);
+				System.out.println("Service type: " + capService);
+				System.out.println("Service minutes used (Night): " + nightMins);
+				System.out.println("Service minutes used (Day): " + dayMins);
+				System.out.println("Amount charged: $" + pChargeForm);
+			}		
 
-if (dayMins > freePremDayMins && nightMins < freePremNightMins) {
-premCharge = premFee + (dayMins - freePremDayMins) * 0.10;
-pChargeForm = String.format("%.2f", premCharge); //formatting the decimal to two places
-System.out.println("Name: " + name);
-System.out.println("Account number: " + accountNum);
-System.out.println("Service type: " + capService);
-System.out.println("Service minutes used (Night): " + nightMins);
-System.out.println("Service minutes used (Day): " + dayMins);
-System.out.println("Amount charged: $" + pChargeForm);
-}
+			if (dayMins < freePremDayMins && nightMins < freePremNightMins) {
+				premCharge = premFee;
+				pChargeForm = String.format("%.2f", premCharge); //formatting the decimal to two places
+				System.out.println("Name: " + name);
+				System.out.println("Account number: " + accountNum);
+				System.out.println("Service type: " + capService);
+				System.out.println("Service minutes used (Night): " + nightMins);
+				System.out.println("Service minutes used (Day): " + dayMins);
+				System.out.println("Amount charged: $" + pChargeForm);
+			}
 
-if (dayMins < freePremDayMins && nightMins > freePremNightMins) {
-premCharge = premFee + (nightMins - freePremNightMins) * 0.05;
-pChargeForm = String.format("%.2f", premCharge); //formatting the decimal to two places
-System.out.println("Name: " + name);
-System.out.println("Account number: " + accountNum);
-System.out.println("Service type: " + capService);
-System.out.println("Service minutes used (Night): " + nightMins);
-System.out.println("Service minutes used (Day): " + dayMins);
-System.out.println("Amount charged: $" + pChargeForm);
-}
+			if (dayMins > freePremDayMins && nightMins < freePremNightMins) {
+				premCharge = premFee + (dayMins - freePremDayMins) * 0.10;
+				pChargeForm = String.format("%.2f", premCharge); //formatting the decimal to two places
+				System.out.println("Name: " + name);
+				System.out.println("Account number: " + accountNum);
+				System.out.println("Service type: " + capService);
+				System.out.println("Service minutes used (Night): " + nightMins);
+				System.out.println("Service minutes used (Day): " + dayMins);
+				System.out.println("Amount charged: $" + pChargeForm);
+			}
 
-} //first "if" statement's bracket
+			if (dayMins < freePremDayMins && nightMins > freePremNightMins) {
+				premCharge = premFee + (nightMins - freePremNightMins) * 0.05;
+				pChargeForm = String.format("%.2f", premCharge); //formatting the decimal to two places
+				System.out.println("Name: " + name);
+				System.out.println("Account number: " + accountNum);
+				System.out.println("Service type: " + capService);
+				System.out.println("Service minutes used (Night): " + nightMins);
+				System.out.println("Service minutes used (Day): " + dayMins);
+				System.out.println("Amount charged: $" + pChargeForm);
+			}
 
-else if (service.equalsIgnoreCase(reg)) { //ignoring case sensitive
-System.out.print("How many day minutes did you use? (No decimals) ");
-dayMins = console.nextInt();
+		} //first "if" statement's bracket
 
-if (dayMins > freeRegMins) {
-regCharge = regFee + (dayMins - freeRegMins) * 0.20;
-rChargeForm = String.format("%.2f", regCharge); //formatting the decimal to two places
-System.out.println("Name: " + name);
-System.out.println("Account number: " + accountNum);
-System.out.println("Service type: " + capService);
-System.out.println("Service minutes used: " + dayMins);
-System.out.println("Amount charged: $" + rChargeForm);
-}
-if (dayMins < freeRegMins) {
-regCharge = regFee;
-rChargeForm = String.format("%.2f", regCharge); //formatting the decimal to two places
-System.out.println("Name: " + name);
-System.out.println("Account number: " + accountNum);
-System.out.println("Service type: " + capService);
-System.out.println("Service minutes used: " + dayMins);
-System.out.println("Amount charged: $" + rChargeForm);
-}
+		else if (service.equalsIgnoreCase(reg)) { //ignoring case sensitive
+			System.out.print("How many day minutes did you use? (No decimals) ");
+			dayMins = console.nextInt();
 
-} //first "else if" statements bracket
+			if (dayMins > freeRegMins) {
+				regCharge = regFee + (dayMins - freeRegMins) * 0.20;
+				rChargeForm = String.format("%.2f", regCharge); //formatting the decimal to two places
+				System.out.println("Name: " + name);
+				System.out.println("Account number: " + accountNum);
+				System.out.println("Service type: " + capService);
+				System.out.println("Service minutes used: " + dayMins);
+				System.out.println("Amount charged: $" + rChargeForm);
+			}
+			if (dayMins < freeRegMins) {
+				regCharge = regFee;
+				rChargeForm = String.format("%.2f", regCharge); //formatting the decimal to two places
+				System.out.println("Name: " + name);
+				System.out.println("Account number: " + accountNum);
+				System.out.println("Service type: " + capService);
+				System.out.println("Service minutes used: " + dayMins);
+				System.out.println("Amount charged: $" + rChargeForm);
+			}
 
-else
-	System.out.println("Service type INVALID. Please try again.");
-		
-console.close(); //closing the console
+		} //first "else if" statements bracket
+
+		else
+			System.out.println("Service type INVALID. Please try again.");
+
+		console.close(); //closing the console
 
 	} //end of main method
 
