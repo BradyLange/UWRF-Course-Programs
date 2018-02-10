51bf4ac819ed6617e2c9966e4ff9a03b8ea4fd5d
Brady Lange <brady.lange111@gmail.com>
2018-02-10 14:39:21 -0600

    Create cylinder.java

---
+++ Programming 2 - CSIS 162/Shape Property Calculations/cylinder.java
@@ -0,0 +1,73 @@
+
+public class cylinder { //start of cylinder class
+	public double area, volume, circumference, radius, height;
+
+	public cylinder() //default constructor
+	{
+		radius = 0;
+		height = 0;
+	}
+
+	public cylinder(double rad, double hei) //alternate constructor 
+	{
+		radius = rad;
+		height = hei;
+	}
+
+	public void calcArea() //area calculation method
+	{
+		area = ((Math.PI * (radius* radius)) * 2) + ((2 * Math.PI * radius) * height);
+	}
+
+	public void calcCircum() //circumference calculation method
+	{
+		circumference = 2 * Math.PI * radius;
+	}
+
+	public void calcVolume() //volume calculation method
+	{
+		volume = Math.PI * (radius * radius) * height;
+	}
+
+	public void setCylinRad(double rad) //setter method
+	{
+		radius = rad;
+	}
+
+	public void setCylinHei(double hei) //setter method
+	{
+		height = hei;
+	}
+
+	public double getCylinRad() //getter method
+	{
+		return radius;
+	}
+
+	public double getCylinHei() //getter method
+	{
+		return height;
+	}
+	
+	public double getCylinArea() //getter method
+	{
+		return area;
+	}
+	
+	public double getCylinCircum() //getter method
+	{
+		return circumference;
+	}
+
+	public double getCylinVol() //getter method
+	{
+		return volume;
+	}
+	
+	public String toString() //toString method
+	{
+		String data = "Circumference = " + circumference + " Area = " + area + " Volume = " + volume + " Radius: " + radius  + " Height: " + height;
+		return data;
+	}
+
+} //end of cylinder class
