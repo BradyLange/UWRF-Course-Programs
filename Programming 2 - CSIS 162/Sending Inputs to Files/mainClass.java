aa68e7c26765fe83a5aaad99e373454711bdb9c6
Brady Lange <brady.lange111@gmail.com>
2018-02-10 14:59:02 -0600

    Update mainClass.java

--- Programming 2 - CSIS 162/Sending Inputs to Files/mainClass.java
+++ Programming 2 - CSIS 162/Sending Inputs to Files/mainClass.java
@@ -1,24 +1,23 @@
 /*
- * Name: Brady Lange
- * Class: CSIS 162 Prgramming II
- * Program 2
+ * Brady Lange
+ * CSIS 162 Prgramming
+ * Program #2
  * Due Date: March 7th, 2017
- * This Java program build objects of type person with states first name, last name, age, GPA, address, GPA average, and mobile number.
- * It consists of two classes: Person and main	 
+ * This Java program builds objects of type person with the states of first name, last name, age, GPA, address, GPA average, and mobile number.
+ * It consists of two classes: Person and mainClass	 
  */
 
 import java.io.*; //importing already coded to allow the program to create input and output files
 import java.util.*;
 
-//main class
+//Main class
 public class mClass { //start of mClass
 
 	public static void main(String[] args) throws FileNotFoundException {
-	
 		
 		//Create object (default constructor)
 		Person csis162 = new Person();
 		
-}//end of main
+	}//end of main
 
 }//end of mClass
