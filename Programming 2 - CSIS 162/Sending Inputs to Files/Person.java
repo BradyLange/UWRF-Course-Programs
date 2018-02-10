9c942323e7f16372b53a9c6706acdd9f08a2a0ca
Brady Lange <brady.lange111@gmail.com>
2018-02-10 15:11:57 -0600

    Update Person.java

--- Programming 2 - CSIS 162/Sending Inputs to Files/Person.java
+++ Programming 2 - CSIS 162/Sending Inputs to Files/Person.java
@@ -1,33 +1,34 @@
 
-/* Name: Brady Lange
- * Class: CSIS 162 Programming II
- * Program 2
+/* Brady Lange
+ * CSIS 162 Programming
+ * Program #2
  * Due Date: March 7th, 2017
  * Class Name: Person
  * States: first name, last name, age, GPA, address, mobile number, GPA average, and sum.
  * This class creates a person object that outputs first name, last name, age, GPA, address, mobile number, and GPA average on an output file.
  */
+
 import java.util.*;
 import java.io.*;
 
 public class Person { //start of Person class 
     
-	//states
-	private String firstName; //to store the first name of person object
+    //States
+    private String firstName; //to store the first name of person object
     private String lastName;  //to store the last name of person object
     private int age;          //age of person object
-    private double GPA; //store grade point average
-    private String address; //store address
+    private double GPA;       //store grade point average
+    private String address;   //store address
     private String mobNumber; //store mobile phone number
     private double gpaAvg; //store the average GPA
     private double sum; //store the GPA sum
        
- //List of constructors
+    //List of constructors
     
     //Default constructor   
     public Person() throws FileNotFoundException
     {
-        firstName = "";
+     	firstName = "";
         lastName = "";
         age = 0;
         GPA = 0;
@@ -36,50 +37,50 @@ public class Person { //start of Person class
         gpaAvg = 0;
         sum = 0;
         
-		//create an input stream
-		Scanner inFile = new Scanner(new FileReader("inFile.txt"));
+	//create an input stream
+	Scanner inFile = new Scanner(new FileReader("inFile.txt"));
 		
-		//create an output stream
-		PrintWriter outFile = new PrintWriter("outData.txt");
+	//create an output stream
+	PrintWriter outFile = new PrintWriter("outData.txt");
 		
-		outFile.println("First\t\tLast\t\tAge\tGPA\tMobile Number\tAddress"); //output file //using tab to format the file to line up its data
-		outFile.println("********************************************************************************************");
+	outFile.println("First\t\tLast\t\tAge\tGPA\tMobile Number\tAddress"); //output file //using tab to format the file to line up its data
+	outFile.println("********************************************************************************************");
 		
-		while(inFile.hasNext()) //hasNext reads to input file (boolean statement)
-		{
-			//get student first name
-			firstName = fName(inFile);
-			outFile.printf("%-8s", firstName);
+	while(inFile.hasNext()) //hasNext reads to input file (boolean statement)
+	{
+		//get student first name
+		firstName = fName(inFile);
+		outFile.printf("%-8s", firstName);
 			
-			lastName = lName(inFile);
-			outFile.printf("%14s", lastName);
+		lastName = lName(inFile);
+		outFile.printf("%14s", lastName);
 			
-			age = age(inFile);
-			outFile.printf("\t\t" + age);
+		age = age(inFile);
+		outFile.printf("\t\t" + age);
 			
-			GPA = GPA(inFile);
-			outFile.printf("\t" + GPA);
-			sum = sum + GPA; //calculate this after GPA receives input that way it will loop and add up all six scores for calculation of the average
+		GPA = GPA(inFile);
+		outFile.printf("\t" + GPA);
+		sum = sum + GPA; //calculate this after GPA receives input that way it will loop and add up all six scores for calculation of the average
 			
-			mobNumber = mobileNumber(inFile);
-			outFile.printf("\t" + mobNumber);
+		mobNumber = mobileNumber(inFile);
+		outFile.printf("\t" + mobNumber);
 			
-			address = address(inFile);
-			outFile.printf("\t" + address);
-			outFile.println(); //generates a different line for each person
+		address = address(inFile);
+		outFile.printf("\t" + address);
+		outFile.println(); //generates a different line for each person
 					
 	
-		} //end of while loop
+	} //end of while loop
 		
-		outFile.println("*********************************************************************************************");
+	outFile.println("*********************************************************************************************");
 		
-		//find the GPA average
-		gpaAvg = sum / 6.0;
-		String cAvgGPA = String.format("%.2f", gpaAvg); //formatting gpa average to two decimal spots
-		outFile.println("There are 6 students in the class and the average GPA for those students is " + cAvgGPA);
+	//find the GPA average
+	gpaAvg = sum / 6.0;
+	String cAvgGPA = String.format("%.2f", gpaAvg); //formatting gpa average to two decimal spots
+	outFile.println("There are 6 students in the class and the average GPA for those students is " + cAvgGPA);
 		
-		inFile.close(); //closing the files
-		outFile.close();
+	inFile.close(); //closing the files
+	outFile.close();
 		 
     } //end of Person
     
@@ -132,21 +133,24 @@ public class Person { //start of Person class
     }
 
 
- //List of setters
+    //List of setters
     
     //setter: sets person last name
-    public void setLastName(String lname){
-    	lastName= lname;
+    public void setLastName(String lname)
+    {
+    	lastName = lname;
     }//end of setLastName
     
     //setter: sets person first name
-    public void setFirstName(String fName){
+    public void setFirstName(String fName)
+    {
     	firstName = fName;
     }//end of setFirstName
     
     //setter: sets person age
-    public void setAge(int myAge){
-    	age=myAge;
+    public void setAge(int myAge)
+    {
+    	age = myAge;
     }//end of setAge
    
     //setter: sets persons GPA
@@ -154,6 +158,7 @@ public class Person { //start of Person class
     {
     	this.GPA = GPA;
     }
+	
     //setter: sets persons address
     public void setAddress(String address)
     {
@@ -167,7 +172,7 @@ public class Person { //start of Person class
     }
 
 
-//list of getters
+    //List of getters
     
     //getter: returns person first name
     public String getFirstName()
@@ -183,7 +188,8 @@ public class Person { //start of Person class
     } //end of getlastName
     
     //Getter: returns person age
-    public int getAge(){
+    public int getAge()
+    {
     	return age;
     }//end of getAge
     
@@ -207,11 +213,11 @@ public class Person { //start of Person class
 
     //toString: prints the states of the object person
    public String toString()
-    {
+   {
       String data = "First name: " + firstName + " Last name: " + lastName + " Age: " + age
     		  + " GPA: " + GPA + " Address: " + address + " Mobile number: " + mobNumber + " GPA Average: " + gpaAvg + " Sum: " + sum;
       return data;
-    } //end of toString
+   } //end of toString
 
 } //end of class person
 
