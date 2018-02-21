64e4589111af06661e98a7b38cbd49b8829220da
Brady Lange <brady.lange111@gmail.com>
2018-02-21 00:20:15 -0600

    Create PointApplication.java

---
+++ Object-Orientated Programming - CSIS 235/Composition with a Circle and Points/PointApplication.java
@@ -0,0 +1,15 @@
+//This class allows the testing of the class Point
+
+public class TestPoint { //start of TestPoint class
+	
+	public static void main (String [] args)
+	{ //start of main method
+		
+		//alternate constructor
+		Point cordinateOne = new Point(5,3);
+		//prints out result
+		System.out.println("Cordinate: " + cordinateOne);
+	
+	} //end of main method
+
+} //end of TestPoint class
