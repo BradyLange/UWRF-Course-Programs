73f3aa7b944adedc49005f5edf9656705ab7ea2d
Brady Lange <brady.lange111@gmail.com>
2018-02-21 00:53:16 -0600

    Create CardInterface.java

---
+++ Object-Orientated Programming - CSIS 235/Generics with Cards/CardInterface.java
@@ -0,0 +1,11 @@
+
+public interface ICard extends Comparable<ICard> {
+	
+	enum Suit {SPADES, HEARTS, DIAMONDS, CLUBS}
+	enum Rank {DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, 
+		JACK, QUEEN, KING, ACE}
+	
+	Suit getSuit();
+	Rank getRank();
+	
+}
