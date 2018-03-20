992229a5e13627d7be4b41b6cb463bad3c297a90
Brady Lange <brady.lange111@gmail.com>
2018-03-20 17:59:00 -0500

    Update NegativeNumberException.java

--- Object-Orientated Programming - CSIS 235/GUI - Right Triangle Validator/NegativeNumberException.java
+++ Object-Orientated Programming - CSIS 235/GUI - Right Triangle Validator/NegativeNumberException.java
@@ -1,9 +1,8 @@
 
-public class NegativeNumberException extends Exception {
-	
-	public NegativeNumberException() {
-		
+public class NegativeNumberException extends Exception 
+{
+	public NegativeNumberException() 
+	{
 		super("Cannot be a negative number.");
-		
 	}
 }
